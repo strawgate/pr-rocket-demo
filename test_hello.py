0ef397d9a30404a3ed29964e25dea3823ec9d511
strawgate <williamseaston@gmail.com>
2026-04-24 01:42:46 -0500

    Add slugify test scenario

--- test_hello.py
+++ test_hello.py
@@ -1,6 +1,6 @@
 """Tests for hello.py."""
 
-from hello import add, greet, subtract
+from hello import add, greet, slugify_title, subtract
 
 
 def test_greet():
@@ -13,3 +13,7 @@ def test_add():
 
 def test_subtract():
     assert subtract(7, 4) == 3
+
+
+def test_slugify_title():
+    assert slugify_title("Hello, World!") == "hello-world"
