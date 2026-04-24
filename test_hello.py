fe97f282ab1b84d68b3c60c25bb9a407b866a003
strawgate <williamseaston@gmail.com>
2026-04-24 11:08:28 -0500

    test: require trimmed greet names

--- test_hello.py
+++ test_hello.py
@@ -7,6 +7,10 @@ def test_greet():
     assert greet("world") == "Hi there, world!"
 
 
+def test_greet_trims_name_whitespace():
+    assert greet("  world  ") == "Hi there, world!"
+
+
 def test_add():
     assert add(2, 3) == 5
 
