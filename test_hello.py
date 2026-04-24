7c721b147f6a57590b47b444429367bc1cfe58da
strawgate <williamseaston@gmail.com>
2026-04-24 11:15:09 -0500

    test: require blank greet fallback

--- test_hello.py
+++ test_hello.py
@@ -7,6 +7,10 @@ def test_greet():
     assert greet("world") == "Hi there, world!"
 
 
+def test_greet_falls_back_to_world_for_blank_name():
+    assert greet("   ") == "Hi there, world!"
+
+
 def test_add():
     assert add(2, 3) == 5
 
